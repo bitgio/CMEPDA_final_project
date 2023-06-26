9038021dc916c67bd57a4e01b3b8ae25276867a5
anapascual <pascual.aranda.ana@gmail.com>
2023-06-26 18:24:42 +0200

    dsdghtfcv

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'nbsphinx', 'sphinx_gallery.load_style']
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = []
