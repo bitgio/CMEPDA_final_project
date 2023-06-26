2a20412c2a7794e36347e16f925d2e6ca33b3126
anapascual <pascual.aranda.ana@gmail.com>
2023-06-26 18:41:21 +0200

    hrdytes

---
+++ prova.py
@@ -0,0 +1,19 @@
+import os
+import threading as thr
+
+
+
+def create_dataset(ls):
+    """Return the square of a given number.
+
+    Here is some text with more in-depth explanation.
+
+    Arguments
+    ---------
+    x : float 
+        The input value
+
+    Return:
+        The square of the input argument.
+    """
+    return 23
