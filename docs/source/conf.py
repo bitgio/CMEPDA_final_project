abdafa5836b6bedc3453601d4a421ed1ed05e2a3
anapascual <pascual.aranda.ana@gmail.com>
2023-06-26 16:50:48 +0200

    Adding nbsphinx extension to conf Sphinx file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'nbsphinx']
 
 templates_path = ['_templates']
 exclude_patterns = []
