1e5564ca11c4a6a12981958c17fdb247408fb326
anapascual <pascual.aranda.ana@gmail.com>
2023-06-28 10:45:39 +0200

    gdxg

---
+++ prova1.py
@@ -0,0 +1,13 @@
+def create_dataset(ls):
+    """ Function calling the Matlab file in order to filter the images
+    
+    Arguments
+    ---------
+    ls : list
+         Chunk of files' directory.
+         
+    
+    Return:
+        Dataset with all images filtered.
+    """
+    return 0
