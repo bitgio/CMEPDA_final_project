6d050eff65c39f3ca33506b3f29bec37a1290de4
anapascual <pascual.aranda.ana@gmail.com>
2023-06-29 20:48:45 +0200

    Notebook to py-file

--- cnn_original.py
+++ cnn_original.py
@@ -116,4 +116,72 @@ if __name__ == '__main__':
     mammo_o_4d = np.reshape(mammo_o, (147, 125, 125, 1))
     mammo_f_4d = np.reshape(mammo_f, (147, 64, 64, 1))
 
+    model_o = cnn_o()
+    model_o.summary()
 
+    learning_rate = 0.001
+    model_o.compile(optimizer = SGD(learning_rate, momentum = 0.9), loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+    reduce_on_plateau = ReduceLROnPlateau(
+    monitor="val_loss",
+    factor=0.1,
+    patience=10,
+    verbose=0,
+    mode="auto",
+    min_delta=0.0001,
+    cooldown=0,
+    min_lr=0)
+
+    X_train_o, X_val_o, Y_train_o, Y_val_o = train_test_split(mammo_o_4d, label, test_size = 0.2, random_state = 44)
+    batch_size = 21
+    traino = model_o.fit(X_train_o, Y_train_o, 
+                     batch_size = batch_size,
+                     epochs = 200, 
+                     verbose=1,
+                     validation_data=(X_val_o, Y_val_o),
+                     callbacks = [reduce_on_plateau])
+    
+    acc = traino.history['accuracy']
+    val_acc = traino.history['val_accuracy']
+    loss = traino.history['loss']
+    val_loss = traino.history['val_loss']
+
+    epochs_range = range(1, len(acc)+1)
+
+    #Train and validation accuracy 
+    plt.figure(figsize=(15, 15))
+    plt.subplot(2, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    #Train and validation loss 
+    plt.subplot(2, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
+
+    _, val_acc = model_o.evaluate(X_val_o, Y_val_o, verbose=0)
+    print('Validation accuracy: %.3f' % (val_acc))
+
+    preds = model_o.predict(X_val_o, verbose=1)
+
+    #Compute Receiver operating characteristic (ROC)
+    fpr, tpr, _ = roc_curve(Y_val_o, preds)
+    roc_auc = auc(fpr, tpr)
+
+    #Plot of a ROC curve
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange', lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
